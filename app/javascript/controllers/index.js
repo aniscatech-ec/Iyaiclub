e682668ffbfa2fa5bdd63a90437235ea378d085b
Titozambrano <titzambralava9@gmail.com>
2026-04-01 16:11:43 -0500

    [MOD] Cambio de carga automática a registro manual de controladores Stimulus [MOD] Reemplazo de eagerLoadControllersFrom por registro explícito de DropdownHoverController [MOD] Limpieza de formato en application.js (nueva línea al final)

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,5 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import DropdownHoverController from "./dropdown_hover_controller"
+
+application.register("dropdown-hover", DropdownHoverController)
